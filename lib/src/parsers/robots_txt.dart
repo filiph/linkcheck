f60115f8db253c9387a5c13acc21ae9761fb7f59
Filip Hracek <filip.hracek@gmail.com>
2016-07-21 21:06:05 -0700

    Add robots parser

---
+++ lib/src/parsers/robots_txt.dart
@@ -0,0 +1,92 @@
+library linkcheck.parsers.html;
+
+class RobotsBouncer {
+  /// The shortest possible identifying part of the user agent.
+  ///
+  /// So, for example for Googlebot it would be "Googlebot", although
+  /// the user agent string is usually longer.
+  final String robotName;
+  final List<_Rule> _rules = new List<_Rule>();
+
+  RobotsBouncer(Iterable<String> lines, {String forRobot: _asterisk})
+      : robotName = forRobot {
+    const userAgentString = "User-agent:";
+    const disallowString = "Disallow:";
+
+    assert(robotName != null);
+
+    Set<String> currentUserAgents = new Set();
+    Set<String> currentPaths = new Set();
+    for (var line in lines) {
+      line = line.trim();
+      if (line.startsWith('#')) continue;
+      if (line.isEmpty) {
+        if (currentUserAgents.isEmpty || currentPaths.isEmpty) {
+          // Invalid rule, discard.
+          currentUserAgents = new Set();
+          currentPaths = new Set();
+          continue;
+        }
+        _rules.add(new _Rule(currentUserAgents, currentPaths));
+        currentUserAgents = new Set();
+        currentPaths = new Set();
+        continue;
+      }
+      if (line.startsWith(userAgentString)) {
+        line = line.substring(userAgentString.length);
+        currentUserAgents.add(line.trim());
+        continue;
+      }
+      if (line.startsWith(disallowString)) {
+        line = line.substring(disallowString.length);
+        currentPaths.add(line.trim());
+        continue;
+      }
+    }
+
+    // Last rule, if any
+    if (currentUserAgents.isNotEmpty && currentPaths.isNotEmpty) {
+      _rules.add(new _Rule(currentUserAgents, currentPaths));
+    }
+  }
+
+  bool allows(String path, {String forRobot}) {
+    String robotName = forRobot ?? this.robotName;
+
+    if (_rules.any((rule) => rule.fitsRobotName(robotName) && rule.allowsAll)) {
+      return true;
+    }
+
+    for (var rule in _rules) {
+      if (rule.fitsRobotName(robotName) && rule.appliesToPath(path)) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+class _Rule {
+  final Set<String> userAgents;
+  final Set<String> paths;
+  _Rule(this.userAgents, this.paths);
+
+  // 'Disallow:' (with empty rulepath) means something like 'allow all'
+  // for fitting bots.
+  bool get allowsAll => paths.any((rulePath) => rulePath.isEmpty);
+
+  bool fitsRobotName(String robotName) {
+    if (userAgents.contains(_asterisk)) return true;
+    robotName = robotName.trim().toLowerCase();
+    return userAgents
+        .map((userAgent) => userAgent.toLowerCase())
+        .any((userAgent) => userAgent.contains(robotName));
+  }
+
+  bool appliesToPath(String path) {
+    if (allowsAll) return false;
+    return paths.any((rulePath) => path.startsWith(rulePath));
+  }
+}
+
+const _asterisk = "*";
---
+++ test/robots_txt_test.dart
@@ -0,0 +1,165 @@
+import 'package:test/test.dart';
+
+import 'package:linkcheck/src/parsers/robots_txt.dart';
+
+void main() {
+  group("robots.txt", () {
+    var robotName = "linkcheck";
+    test("disallow everything", () {
+      List<String> contents = """
+      User-agent: *
+      Disallow: /
+      """
+          .split("\n")
+          .map((line) => line.trim())
+          .toList(growable: false);
+      var robots = new RobotsBouncer(contents, forRobot: robotName);
+
+      expect(robots.allows("/"), isFalse);
+      expect(robots.allows("/#something"), isFalse);
+      expect(robots.allows("/?query=something"), isFalse);
+      expect(robots.allows("/something"), isFalse);
+      expect(robots.allows("/something/other"), isFalse);
+    });
+
+    test("allow everything (empty disallow)", () {
+      List<String> contents = """
+      User-agent: *
+      Disallow:
+      """
+          .split("\n")
+          .map((line) => line.trim())
+          .toList(growable: false);
+      var robots = new RobotsBouncer(contents, forRobot: robotName);
+
+      expect(robots.allows("/"), isTrue);
+      expect(robots.allows("/#something"), isTrue);
+      expect(robots.allows("/?query=something"), isTrue);
+      expect(robots.allows("/something"), isTrue);
+      expect(robots.allows("/something/other"), isTrue);
+    });
+
+    test("allow everything (empty disallow, no last line)", () {
+      List<String> contents = """
+      User-agent: *
+      Disallow:"""
+          .split("\n")
+          .map((line) => line.trim())
+          .toList(growable: false);
+      var robots = new RobotsBouncer(contents, forRobot: robotName);
+
+      expect(robots.allows("/"), isTrue);
+      expect(robots.allows("/#something"), isTrue);
+      expect(robots.allows("/?query=something"), isTrue);
+      expect(robots.allows("/something"), isTrue);
+      expect(robots.allows("/something/other"), isTrue);
+    });
+
+    test("allow everything (empty file)", () {
+      List<String> contents = """
+      """
+          .split("\n")
+          .map((line) => line.trim())
+          .toList(growable: false);
+      var robots = new RobotsBouncer(contents, forRobot: robotName);
+
+      expect(robots.allows("/"), isTrue);
+      expect(robots.allows("/#something"), isTrue);
+      expect(robots.allows("/?query=something"), isTrue);
+      expect(robots.allows("/something"), isTrue);
+      expect(robots.allows("/something/other"), isTrue);
+    });
+
+    test("disallow subdirectory", () {
+      List<String> contents = """
+      User-agent: *
+      Disallow: /something/
+      """
+          .split("\n")
+          .map((line) => line.trim())
+          .toList(growable: false);
+      var robots = new RobotsBouncer(contents, forRobot: robotName);
+
+      expect(robots.allows("/"), isTrue);
+      expect(robots.allows("/#something"), isTrue);
+      expect(robots.allows("/?query=something"), isTrue);
+      expect(robots.allows("/something"), isTrue);
+      expect(robots.allows("/something/other"), isFalse);
+    });
+
+    test("disallow a few different files", () {
+      List<String> contents = """
+      User-agent: *
+      Disallow: /~joe/junk.html
+      Disallow: /~joe/foo.html
+      Disallow: /~joe/bar.html
+      """
+          .split("\n")
+          .map((line) => line.trim())
+          .toList(growable: false);
+      var robots = new RobotsBouncer(contents, forRobot: robotName);
+
+      expect(robots.allows("/"), isTrue);
+      expect(robots.allows("/#something"), isTrue);
+      expect(robots.allows("/~joe/junk.html"), isFalse);
+      expect(robots.allows("/~joe/foo.html"), isFalse);
+      expect(robots.allows("/~joe/bar.html"), isFalse);
+      expect(robots.allows("/~joe/bar.html#"), isFalse);
+      expect(robots.allows("/~joe/baz.html"), isTrue);
+    });
+
+    test("disallow doesn't apply for different bot", () {
+      List<String> contents = """
+      User-agent: BadBot
+      Disallow: /
+      """
+          .split("\n")
+          .map((line) => line.trim())
+          .toList(growable: false);
+      var robots = new RobotsBouncer(contents, forRobot: robotName);
+
+      expect(robots.allows("/"), isTrue);
+      expect(robots.allows("/#something"), isTrue);
+      expect(robots.allows("/?query=something"), isTrue);
+      expect(robots.allows("/something"), isTrue);
+      expect(robots.allows("/something/other"), isTrue);
+    });
+
+    test("disallow applies for matching bot", () {
+      List<String> contents = """
+      User-agent: $robotName
+      Disallow: /something/
+      """
+          .split("\n")
+          .map((line) => line.trim())
+          .toList(growable: false);
+      var robots = new RobotsBouncer(contents, forRobot: robotName);
+
+      expect(robots.allows("/"), isTrue);
+      expect(robots.allows("/#something"), isTrue);
+      expect(robots.allows("/?query=something"), isTrue);
+      expect(robots.allows("/something"), isTrue);
+      expect(robots.allows("/something/other"), isFalse);
+    });
+
+    test("allows a single matching bot", () {
+      List<String> contents = """
+      User-agent: $robotName
+      Disallow:
+
+      User-agent: *
+      Disallow: /
+      """
+          .split("\n")
+          .map((line) => line.trim())
+          .toList(growable: false);
+      var robots = new RobotsBouncer(contents, forRobot: robotName);
+
+      expect(robots.allows("/"), isTrue);
+      expect(robots.allows("/#something"), isTrue);
+      expect(robots.allows("/?query=something"), isTrue);
+      expect(robots.allows("/something"), isTrue);
+      expect(robots.allows("/something/other"), isTrue);
+    });
+  });
+}
