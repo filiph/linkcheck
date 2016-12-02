adbf3842a6ad9d0e190d40a3276fe6ec6f31a4d9
Filip Hracek <filip.hracek@gmail.com>
2016-12-02 11:04:32 -0800

    Catch all errors in the main executable

--- bin/linkcheck.dart
+++ bin/linkcheck.dart
@@ -6,7 +6,16 @@ import 'dart:io';
 import 'package:linkcheck/linkcheck.dart';
 
 Future<int> main(List<String> arguments) async {
-  // Run the link checker. The returned value will be the program's exit code.
-  exitCode = await run(arguments, stdout);
+  runZoned(() async {
+    // Run the link checker. The returned value will be the program's exit code.
+    exitCode = await run(arguments, stdout);
+  }, onError: (e) {
+    stderr.writeln("INTERNAL ERROR: Sorry! Please open "
+        "https://github.com/filiph/linkcheck/issues/new "
+        "in your favorite browser and copy paste the following output there:"
+        "\n");
+    stderr.writeln(e.toString());
+    exitCode = 2;
+  });
   return exitCode;
 }
