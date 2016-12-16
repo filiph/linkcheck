f9d13281679a6613e11e005c3c22b976aaf083e5
Filip Hracek <filip.hracek@gmail.com>
2016-12-15 16:03:29 -0800

    Minor no-op changes

--- test/glob_test.dart
+++ test/glob_test.dart
@@ -4,32 +4,30 @@ import 'dart:async';
 import 'package:linkcheck/src/worker/fetch_options.dart';
 
 void main() {
-  group("Glob", () {
-    test("parses simple example", () {
-      var sink = new StreamController<Map>();
-      var options = new FetchOptions(sink);
-      Uri uri = Uri.parse("http://localhost:4000/");
-      options.addHostGlobs([uri.toString() + "**"]);
-      expect(options.matchesAsInternal(uri), isTrue);
-      sink.close();
-    });
+  test("parses simple example", () {
+    var sink = new StreamController<Map>();
+    var options = new FetchOptions(sink);
+    Uri uri = Uri.parse("http://localhost:4000/");
+    options.addHostGlobs([uri.toString() + "**"]);
+    expect(options.matchesAsInternal(uri), isTrue);
+    sink.close();
+  });
 
-    test("parses localhost:4000/guides", () {
-      var sink = new StreamController<Map>();
-      var options = new FetchOptions(sink);
-      Uri uri = Uri.parse("http://localhost:4000/guides");
-      options.addHostGlobs([uri.toString() + "**"]);
-      expect(options.matchesAsInternal(uri), isTrue);
-      sink.close();
-    });
+  test("parses localhost:4000/guides", () {
+    var sink = new StreamController<Map>();
+    var options = new FetchOptions(sink);
+    Uri uri = Uri.parse("http://localhost:4000/guides");
+    options.addHostGlobs([uri.toString() + "**"]);
+    expect(options.matchesAsInternal(uri), isTrue);
+    sink.close();
+  });
 
-    test("parses localhost:4000/guides/", () {
-      var sink = new StreamController<Map>();
-      var options = new FetchOptions(sink);
-      Uri uri = Uri.parse("http://localhost:4000/guides/");
-      options.addHostGlobs(["http://localhost:4000/guides**"]);
-      expect(options.matchesAsInternal(uri), isTrue);
-      sink.close();
-    });
+  test("parses localhost:4000/guides/", () {
+    var sink = new StreamController<Map>();
+    var options = new FetchOptions(sink);
+    Uri uri = Uri.parse("http://localhost:4000/guides/");
+    options.addHostGlobs(["http://localhost:4000/guides**"]);
+    expect(options.matchesAsInternal(uri), isTrue);
+    sink.close();
   });
 }
--- test/robots_txt_test.dart
+++ test/robots_txt_test.dart
@@ -3,163 +3,161 @@ import 'package:test/test.dart';
 import 'package:linkcheck/src/parsers/robots_txt.dart';
 
 void main() {
-  group("robots.txt", () {
-    var robotName = "linkcheck";
-    test("disallow everything", () {
-      List<String> contents = """
+  var robotName = "linkcheck";
+  test("disallow everything", () {
+    List<String> contents = """
       User-agent: *
       Disallow: /
       """
-          .split("\n")
-          .map((line) => line.trim())
-          .toList(growable: false);
-      var robots = new RobotsBouncer(contents, forRobot: robotName);
+        .split("\n")
+        .map((line) => line.trim())
+        .toList(growable: false);
+    var robots = new RobotsBouncer(contents, forRobot: robotName);
 
-      expect(robots.allows("/"), isFalse);
-      expect(robots.allows("/#something"), isFalse);
-      expect(robots.allows("/?query=something"), isFalse);
-      expect(robots.allows("/something"), isFalse);
-      expect(robots.allows("/something/other"), isFalse);
-    });
+    expect(robots.allows("/"), isFalse);
+    expect(robots.allows("/#something"), isFalse);
+    expect(robots.allows("/?query=something"), isFalse);
+    expect(robots.allows("/something"), isFalse);
+    expect(robots.allows("/something/other"), isFalse);
+  });
 
-    test("allow everything (empty disallow)", () {
-      List<String> contents = """
+  test("allow everything (empty disallow)", () {
+    List<String> contents = """
       User-agent: *
       Disallow:
       """
-          .split("\n")
-          .map((line) => line.trim())
-          .toList(growable: false);
-      var robots = new RobotsBouncer(contents, forRobot: robotName);
+        .split("\n")
+        .map((line) => line.trim())
+        .toList(growable: false);
+    var robots = new RobotsBouncer(contents, forRobot: robotName);
 
-      expect(robots.allows("/"), isTrue);
-      expect(robots.allows("/#something"), isTrue);
-      expect(robots.allows("/?query=something"), isTrue);
-      expect(robots.allows("/something"), isTrue);
-      expect(robots.allows("/something/other"), isTrue);
-    });
+    expect(robots.allows("/"), isTrue);
+    expect(robots.allows("/#something"), isTrue);
+    expect(robots.allows("/?query=something"), isTrue);
+    expect(robots.allows("/something"), isTrue);
+    expect(robots.allows("/something/other"), isTrue);
+  });
 
-    test("allow everything (empty disallow, no last line)", () {
-      List<String> contents = """
+  test("allow everything (empty disallow, no last line)", () {
+    List<String> contents = """
       User-agent: *
       Disallow:"""
-          .split("\n")
-          .map((line) => line.trim())
-          .toList(growable: false);
-      var robots = new RobotsBouncer(contents, forRobot: robotName);
+        .split("\n")
+        .map((line) => line.trim())
+        .toList(growable: false);
+    var robots = new RobotsBouncer(contents, forRobot: robotName);
 
-      expect(robots.allows("/"), isTrue);
-      expect(robots.allows("/#something"), isTrue);
-      expect(robots.allows("/?query=something"), isTrue);
-      expect(robots.allows("/something"), isTrue);
-      expect(robots.allows("/something/other"), isTrue);
-    });
+    expect(robots.allows("/"), isTrue);
+    expect(robots.allows("/#something"), isTrue);
+    expect(robots.allows("/?query=something"), isTrue);
+    expect(robots.allows("/something"), isTrue);
+    expect(robots.allows("/something/other"), isTrue);
+  });
 
-    test("allow everything (empty file)", () {
-      List<String> contents = """
+  test("allow everything (empty file)", () {
+    List<String> contents = """
       """
-          .split("\n")
-          .map((line) => line.trim())
-          .toList(growable: false);
-      var robots = new RobotsBouncer(contents, forRobot: robotName);
+        .split("\n")
+        .map((line) => line.trim())
+        .toList(growable: false);
+    var robots = new RobotsBouncer(contents, forRobot: robotName);
 
-      expect(robots.allows("/"), isTrue);
-      expect(robots.allows("/#something"), isTrue);
-      expect(robots.allows("/?query=something"), isTrue);
-      expect(robots.allows("/something"), isTrue);
-      expect(robots.allows("/something/other"), isTrue);
-    });
+    expect(robots.allows("/"), isTrue);
+    expect(robots.allows("/#something"), isTrue);
+    expect(robots.allows("/?query=something"), isTrue);
+    expect(robots.allows("/something"), isTrue);
+    expect(robots.allows("/something/other"), isTrue);
+  });
 
-    test("disallow subdirectory", () {
-      List<String> contents = """
+  test("disallow subdirectory", () {
+    List<String> contents = """
       User-agent: *
       Disallow: /something/
       """
-          .split("\n")
-          .map((line) => line.trim())
-          .toList(growable: false);
-      var robots = new RobotsBouncer(contents, forRobot: robotName);
+        .split("\n")
+        .map((line) => line.trim())
+        .toList(growable: false);
+    var robots = new RobotsBouncer(contents, forRobot: robotName);
 
-      expect(robots.allows("/"), isTrue);
-      expect(robots.allows("/#something"), isTrue);
-      expect(robots.allows("/?query=something"), isTrue);
-      expect(robots.allows("/something"), isTrue);
-      expect(robots.allows("/something/other"), isFalse);
-    });
+    expect(robots.allows("/"), isTrue);
+    expect(robots.allows("/#something"), isTrue);
+    expect(robots.allows("/?query=something"), isTrue);
+    expect(robots.allows("/something"), isTrue);
+    expect(robots.allows("/something/other"), isFalse);
+  });
 
-    test("disallow a few different files", () {
-      List<String> contents = """
+  test("disallow a few different files", () {
+    List<String> contents = """
       User-agent: *
       Disallow: /~joe/junk.html
       Disallow: /~joe/foo.html
       Disallow: /~joe/bar.html
       """
-          .split("\n")
-          .map((line) => line.trim())
-          .toList(growable: false);
-      var robots = new RobotsBouncer(contents, forRobot: robotName);
+        .split("\n")
+        .map((line) => line.trim())
+        .toList(growable: false);
+    var robots = new RobotsBouncer(contents, forRobot: robotName);
 
-      expect(robots.allows("/"), isTrue);
-      expect(robots.allows("/#something"), isTrue);
-      expect(robots.allows("/~joe/junk.html"), isFalse);
-      expect(robots.allows("/~joe/foo.html"), isFalse);
-      expect(robots.allows("/~joe/bar.html"), isFalse);
-      expect(robots.allows("/~joe/bar.html#"), isFalse);
-      expect(robots.allows("/~joe/baz.html"), isTrue);
-    });
+    expect(robots.allows("/"), isTrue);
+    expect(robots.allows("/#something"), isTrue);
+    expect(robots.allows("/~joe/junk.html"), isFalse);
+    expect(robots.allows("/~joe/foo.html"), isFalse);
+    expect(robots.allows("/~joe/bar.html"), isFalse);
+    expect(robots.allows("/~joe/bar.html#"), isFalse);
+    expect(robots.allows("/~joe/baz.html"), isTrue);
+  });
 
-    test("disallow doesn't apply for different bot", () {
-      List<String> contents = """
+  test("disallow doesn't apply for different bot", () {
+    List<String> contents = """
       User-agent: BadBot
       Disallow: /
       """
-          .split("\n")
-          .map((line) => line.trim())
-          .toList(growable: false);
-      var robots = new RobotsBouncer(contents, forRobot: robotName);
+        .split("\n")
+        .map((line) => line.trim())
+        .toList(growable: false);
+    var robots = new RobotsBouncer(contents, forRobot: robotName);
 
-      expect(robots.allows("/"), isTrue);
-      expect(robots.allows("/#something"), isTrue);
-      expect(robots.allows("/?query=something"), isTrue);
-      expect(robots.allows("/something"), isTrue);
-      expect(robots.allows("/something/other"), isTrue);
-    });
+    expect(robots.allows("/"), isTrue);
+    expect(robots.allows("/#something"), isTrue);
+    expect(robots.allows("/?query=something"), isTrue);
+    expect(robots.allows("/something"), isTrue);
+    expect(robots.allows("/something/other"), isTrue);
+  });
 
-    test("disallow applies for matching bot", () {
-      List<String> contents = """
+  test("disallow applies for matching bot", () {
+    List<String> contents = """
       User-agent: $robotName
       Disallow: /something/
       """
-          .split("\n")
-          .map((line) => line.trim())
-          .toList(growable: false);
-      var robots = new RobotsBouncer(contents, forRobot: robotName);
+        .split("\n")
+        .map((line) => line.trim())
+        .toList(growable: false);
+    var robots = new RobotsBouncer(contents, forRobot: robotName);
 
-      expect(robots.allows("/"), isTrue);
-      expect(robots.allows("/#something"), isTrue);
-      expect(robots.allows("/?query=something"), isTrue);
-      expect(robots.allows("/something"), isTrue);
-      expect(robots.allows("/something/other"), isFalse);
-    });
+    expect(robots.allows("/"), isTrue);
+    expect(robots.allows("/#something"), isTrue);
+    expect(robots.allows("/?query=something"), isTrue);
+    expect(robots.allows("/something"), isTrue);
+    expect(robots.allows("/something/other"), isFalse);
+  });
 
-    test("allows a single matching bot", () {
-      List<String> contents = """
+  test("allows a single matching bot", () {
+    List<String> contents = """
       User-agent: $robotName
       Disallow:
 
       User-agent: *
       Disallow: /
       """
-          .split("\n")
-          .map((line) => line.trim())
-          .toList(growable: false);
-      var robots = new RobotsBouncer(contents, forRobot: robotName);
+        .split("\n")
+        .map((line) => line.trim())
+        .toList(growable: false);
+    var robots = new RobotsBouncer(contents, forRobot: robotName);
 
-      expect(robots.allows("/"), isTrue);
-      expect(robots.allows("/#something"), isTrue);
-      expect(robots.allows("/?query=something"), isTrue);
-      expect(robots.allows("/something"), isTrue);
-      expect(robots.allows("/something/other"), isTrue);
-    });
+    expect(robots.allows("/"), isTrue);
+    expect(robots.allows("/#something"), isTrue);
+    expect(robots.allows("/?query=something"), isTrue);
+    expect(robots.allows("/something"), isTrue);
+    expect(robots.allows("/something/other"), isTrue);
   });
 }
