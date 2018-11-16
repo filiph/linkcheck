068a527256a4759cf03bc7fbdeedcb357031c3dc
Filip Hracek <filip.hracek@gmail.com>
2018-11-16 09:55:08 -0800

    Bump version

--- lib/linkcheck.dart
+++ lib/linkcheck.dart
@@ -25,7 +25,7 @@ const helpFlag = "help";
 const hostsFlag = "hosts";
 const inputFlag = "input-file";
 const skipFlag = "skip-file";
-const version = "2.0.4";
+const version = "2.0.5";
 const versionFlag = "version";
 final _portOnlyRegExp = new RegExp(r"^:\d+$");
 
