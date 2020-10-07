d8d338a57593bd851980e49439ab0cfd0cb14973
Filip Hracek <filip.hracek@gmail.com>
2020-10-06 17:04:09 -0700

    Bump version

--- lib/linkcheck.dart
+++ lib/linkcheck.dart
@@ -28,7 +28,7 @@ const hostsFlag = "hosts";
 const inputFlag = "input-file";
 const redirectFlag = "show-redirects";
 const skipFlag = "skip-file";
-const version = "2.0.13";
+const version = "2.0.14";
 const versionFlag = "version";
 final _portOnlyRegExp = RegExp(r"^:\d+$");
 
