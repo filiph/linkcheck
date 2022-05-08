630d09a4e997c7fceb951626e67a9aeedf2acdd3
tooomm <tooomm@users.noreply.github.com>
2022-05-08 11:11:17 +0200

    bump version

--- lib/linkcheck.dart
+++ lib/linkcheck.dart
@@ -28,7 +28,7 @@ const hostsFlag = "hosts";
 const inputFlag = "input-file";
 const redirectFlag = "show-redirects";
 const skipFlag = "skip-file";
-const version = "2.0.20";
+const version = "2.0.21";
 const versionFlag = "version";
 final _portOnlyRegExp = RegExp(r"^:\d+$");
 
